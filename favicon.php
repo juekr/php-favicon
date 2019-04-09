bb7c79f6819b79fcd5f6c39f2548402489287d2f
I. Gaffling <10500034+gaffling@users.noreply.github.com>
2019-04-09 10:21:18 +0200

    Update favicon.php

--- favicon.php
+++ favicon.php
@@ -1,24 +1,44 @@
 <?php
 
-/** 
- * PHP-dynamic-Counter-Favicon (maximum two digits)
- *
- * Copyright 2019 Igor Gaffling
- *
- * Idea is based on this Article:
- * https://viralpatel.net/blogs/dynamic-unread-count-favicon-in-php/
- * Written by Viral Patel
- *
- * Free for any profit or non-profit use.
- * You may freely distribute or modify this code.
- *
- * Use:
- * <link href="favicon.php?char=27" rel="icon" type="image/png">
- *
- */
+/*
 
-/* Read the favicon template from frame.png file from current directory */
-$im = imagecreatefrompng('frame.png'); // Maybe change Font Color if you use an other Image
+PHP-dynamic-Counter-Favicon (maximum two digits)
+================================================
+
+> PHP Dynamic Content Favicon shows e.g. unread email counts or whatever, direct in your browser tab icon. If your browser has lots of tabs open, this might be a really useful feature that lets user know of any count item. This small but powerful script in PHP let you create your own dynamic Favicon. It will use the PHP GD library to manipulate the favicon image and add text (maximum two digits) into it. If you try to give a longer Text to the Script it will show three dots in the icon (...) - thats all ;-)
+
+**Idea based on this Article:**<br>
+https://viralpatel.net/blogs/dynamic-unread-count-favicon-in-php/<br>
+written by Viral Patel
+
+**Use:**
+
+```html
+<link href="favicon.php?char=27" rel="icon" type="image/png">
+```
+
+or
+
+```PHP
+<link href="favicon.php?char=<?php echo $char; ?>" rel="icon" type="image/png">
+```
+
+**Try:**<br>
+test.php
+
+**Info:**<br>
+https://github.com/audreyr/favicon-cheat-sheet
+
+***Free for any profit or non-profit use.<br>
+You may freely distribute or modify this code.***
+
+Copyright 2019 Igor Gaffling
+
+*/
+
+/* Read the favicon template from png file from current directory */
+/* frame-border.png / frame-card.png / frame-cal.php */
+$im = imagecreatefrompng('frame-border.png'); // Maybe change Font Color if you use an other Image
 
 /* Read the Character which needs to be added in Favicon from Request */
 if ( isset($_REQUEST['char']) && !empty($_REQUEST['char']) ) {
